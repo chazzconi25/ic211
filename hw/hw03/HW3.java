fe7dc2caa526ec800118f1c97997be742f13529a
Charlie Francesconi <m251806@usna.edu>
2023-01-22 23:44:22 -0500

    add comments

--- hw/hw03/HW3.java
+++ hw/hw03/HW3.java
@@ -1,6 +1,16 @@
+/*
+ * This program takes in a number of mids from the user and
+ * their information. The user is then prompted for a company
+ * and the program prints all of the mids in that company.
+ */
 import java.util.*;
 
 public class HW3 {
+    /*
+    * Mid object constructor
+    * @param Scanner in takes in user infromation on Mids
+    * @return Mid object with assigned fields
+    */
     public static Mid createMid(Scanner in) {
         Mid x = new Mid();
         System.out.print("Alpha? ");
@@ -13,10 +23,22 @@ public class HW3 {
         x.company = in.nextInt();
         return x;
     }
-    public static void printMid(Mid x) {
-        System.out.println(x.alpha + " " + x.lastName + " " + x.firstName + " " + x.company);
-    }
 
+    /*
+     * prints out information for a given mid
+     * @param Mid x is the mid object to be prited
+     */
+    public static void printMid(Mid x) {
+        System.out.println(x.alpha + " " + x.lastName + " " + x.firstName + 
+        " " + x.company);
+    }
+    
+    /*
+     * takes in a number of Mids and allows the user
+     * to enter inforamtion for each mid and query them by
+     * number.
+     * @param args not implemented
+     */
     public static void main(String [] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("How many mids? ");
