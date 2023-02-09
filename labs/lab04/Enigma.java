8f2021cd0f857ef1609e20ca6e92a374b3eee672
Charlie Francesconi <m251806@usna.edu>
2023-02-09 00:44:29 -0500

    added some comments

--- labs/lab04/Enigma.java
+++ labs/lab04/Enigma.java
@@ -1,4 +1,11 @@
+/**
+ * Enigma class represents a full enigma machiene. The user can select
+ * from 5 default rotors and choose starting positons on 3 of them
+ * to encrypt or decrypt messages.
+ */
 public class Enigma {
+
+    // fields 
     final String[] DEFAULT_ROTORS = new String[] {
         "#GNUAHOVBIPWCJQXDKRYELSZFMT", "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY", "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
@@ -9,13 +16,31 @@ public class Enigma {
     Rotor mid;
     Rotor inner;
 
+    /**
+     * Constructs a new Engima machiene given three default rotor
+     * numbers and a string representing the starting positions of
+     * those rotors
+     * @param id1 the number of the first default rotor to be used as
+     * the inner rotor on the enigma machiene
+     * @param id2 the number of the first default rotor to be used as
+     * the mid rotor on the enigma machiene
+     * @param id3 the number of the first default rotor to be used as
+     * the outer rotor on the enigma machiene
+     * @param starts the starting characters of each rotor in the
+     * order they were passed to the Enigma constructor
+     */
     Enigma(int id1, int id2, int id3, String starts) {
         inner = new Rotor(DEFAULT_ROTORS[id1-1], starts.charAt(0));
         mid = new Rotor(DEFAULT_ROTORS[id2-1], starts.charAt(1));
         outer = new Rotor(DEFAULT_ROTORS[id3-1], starts.charAt(2));
     }
 
-
+    /**
+     * Encrypts a given message in enigma using the supplied rotors
+     * and starting positions of those rotors
+     * @param message message to be encrypted
+     * @return encrypted message
+     */
     public String encrypt(String message) {
         String encrypted = "";
         for(int i = 0; i < message.length(); i++) {
@@ -31,32 +56,56 @@ public class Enigma {
         }
         return encrypted;
     }
-    private char encryptChar(char a) {
-        char one = outer.letterAtPos(inner.rotorPos(a));
-        char two = outer.letterAtPos(mid.rotorPos(one));
-        return two;
-    }
-    private char decryptChar(char a) {
-        char one = mid.letterAtPos(outer.rotorPos(a));
-        char two = inner.letterAtPos(outer.rotorPos(one));
-        return two;
-    }
 
+    /**
+     * Decrypts a given message in enigma using the supplied rotors
+     * and starting positions of those rotors
+     * @param message message to be decrypted
+     * @return decrypted message
+     */
     public String decrypt(String message) {
         String decrypted = "";
         for(int i = 0; i < message.length(); i++) {
             decrypted += decryptChar(message.charAt(i));
-            inner.rotateClockwise(26);
+            inner.rotateClockwise(1);
             if((i + 1) % 27 == 0) {
-                mid.rotateClockwise(26);
+                mid.rotateClockwise(1);
             }
             if((i + 1) % (27*27) == 0) {
-                outer.rotateClockwise(26);
+                outer.rotateClockwise(1);
             }
         }
         return decrypted;
     }
 
+    /**
+     * Encrypts a single letter using the rotors and starting
+     * positions of this Enigma machinene.
+     * @param a letter to be encrypted
+     * @return encrypted letter
+     */
+    private char encryptChar(char a) {
+        char one = outer.letterAtPos(inner.rotorPos(a));
+        char two = outer.letterAtPos(mid.rotorPos(one));
+        return two;
+    }
+
+    /**
+     * Decrypts a single letter using the rotors and starting
+     * positions of this Enigma machinene.
+     * @param a letter to be decrypted
+     * @return decrypted letter
+     */
+    private char decryptChar(char a) {
+        char one = mid.letterAtPos(outer.rotorPos(a));
+        char two = inner.letterAtPos(outer.rotorPos(one));
+        return two;
+    }
+    
+    /**
+     * Tests the machene by encrypting a simple message
+     * @param args command line arguments. Unused here.
+     */
     public static void main(String [] args) {
         Enigma test = new Enigma(1,2,3,"###");
         System.out.println(test.encrypt("AAA"));
--- labs/lab04/Rotor.java
+++ labs/lab04/Rotor.java
@@ -1,26 +1,64 @@
+/**
+ * Rotor class represents one rotor on an ingma machine. The rotor
+ * can be searched for a specific letter and its position or find
+ * a letter given a position. The rotor can also be rotated clockwise.
+ */
 public class Rotor {
+
+    // Fields
     String state;
 
+    /**
+     * Constuctor for a rotor that represents a rotor on an Enigma
+     * machiene. The state String is all of the characters on the
+     * rotor and the char top represents the letter on the rotor that
+     * is on top when the rotor is initalized
+     * @param state the current rotation of the rotor
+     * @param top the inital starting letter of the rotor
+     */
     public Rotor(String state, char top) {
         this.state = state;
         int turns = state.indexOf(top);
         this.rotateClockwise(state.length() - turns);
     }
 
+    /**
+     * Adjusts the state string in this Rotor to represent turn(s)
+     * clockwise
+     * @param turns the amount of times the rotor should be roated
+     * clockwise
+     */
     public void rotateClockwise(int turns) {
         int len = state.length();
         turns = turns%len;
-        this.state = state.substring(len - turns) + state.substring(0, len - turns);
+        this.state = state.substring(len - turns) 
+                    + state.substring(0, len - turns);
     }
 
+    /**
+     * Returns the index of a letter on the Rotor's state
+     * @param find character to find on the Rotor
+     * @return the index of find in this Rotor's state
+     */
     public int rotorPos(char find) {
         return state.indexOf(find);
     }
 
+
+    /**
+     * Returns the letter at a specificed index of the Rotor's state
+     * @param index index on the rotor to search for a letter
+     * @return letter at the given index
+     */
     public char letterAtPos(int index) {
         return state.charAt(index);
     }
 
+    /**
+     * Used to debug Rotor.java - tests rotating a Rotor multiple
+     * times
+     * @param args command line arguments. Unused here.
+     */
     public static void main(String [] args) {
         Rotor test = new Rotor("#GNUAHOVBIPWCJQXDKRYELSZFMT", '#');
         System.out.println(test.state);
