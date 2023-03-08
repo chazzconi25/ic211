a28d6699646e8ebd6a9f8c3bebada3a2cda82519
chazzconi25 <m251806@usna.edu>
2023-03-07 22:06:49 -0500

    finished homeowrk 9

--- hw/hw09/HW09.java
+++ hw/hw09/HW09.java
@@ -8,14 +8,22 @@ public class HW09 {
     if (verbose) {
       System.out.print("Enter x, k, m: ");
     }
-    int x = sc.nextInt();
-    int k = sc.nextInt();
-    int m = sc.nextInt();
-    int r = MyMath.modexp(x, k, m);
-
-    if (verbose) {
-      System.out.print(x + "^" + k + " % " + m + " = ");
-    }
+    try {
+        int x = sc.nextInt();
+        int k = sc.nextInt();
+        if(k < 0) {
+            throw new Exception();
+        }
+        int m = sc.nextInt();
+        int r = MyMath.modexp(x, k, m);
+        if (verbose) {
+            System.out.print(x + "^" + k + " % " + m + " = ");
+        }
     System.out.println(r);
+    } catch(Exception e) {
+        if (verbose) {
+            System.out.println("Error in HW09! invalid input.");
+        }
+    }
   }
 }
