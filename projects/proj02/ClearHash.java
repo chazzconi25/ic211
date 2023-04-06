bad98656dfa794bab19e783bb46a162eece67dac
chazzconi25 <m251806@usna.edu>
2023-04-06 00:00:12 -0400

    added comments

--- projects/proj02/ClearHash.java
+++ projects/proj02/ClearHash.java
@@ -1,12 +1,34 @@
+/**
+ * This class implments hasher to allow clear hashing
+ * @author Fncsoiances, Charles
+ */
 public class ClearHash implements Hasher {
     private Encryptor E = new Clear();
     private String key;
+
+    /**
+     * Returns the name of this hash
+     * @return the name of this hash
+     */
     public String getHashName() {return "clear";}
+
+    /**
+     * Initializes the hash function and Encryptor
+     * @param key Used to initalize the encryptor E attached to this Hasher
+     * @throws InvalidCharException Exception if an invalid char is passed
+     * to the key
+     */
     public void init(char[] key) throws InvalidCharException {
         this.key = new String(key);
         E.init(key);
     }
 
+    /**
+     * Produces a hash based on the Encryptor initalized with a key
+     * @return hashed value for Encryptor base on key
+     * @throws InvalidCharException Exception if an invalid char is passed
+     * to the key
+     */
     public String hash() throws InvalidCharException {
         String encrypted =  E.encrypt(key);
         char[] hash = new char[16];
